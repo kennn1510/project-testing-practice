72dca758fc7f694b21efc86ed9bbb3a8997d7ade
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-03-01 14:33:16 -0600

    Add test for subtract function

--- testing-practice/testing.test.js
+++ testing-practice/testing.test.js
@@ -1,13 +1,17 @@
-import {capitalize, reverseString, calculator} from "./testing";
+import { capitalize, reverseString, calculator } from "./testing";
 
 test("capitalize function takes a string and returns first character capitalized", () => {
-    expect(capitalize("hello")).toBe("Hello");
+  expect(capitalize("hello")).toBe("Hello");
 });
 
 test("Reverses string", () => {
-    expect(reverseString("hello")).toBe("olleh");
+  expect(reverseString("hello")).toBe("olleh");
 });
 
 test("Calculator adds correctly", () => {
-    expect(calculator.add(2, 2)).toBe(4);
-})
+  expect(calculator.add(2, 2)).toBe(4);
+});
+
+test("Calculator subtracts correctly", () => {
+  expect(calculator.subtract(2, 2)).toBe(0);
+});
