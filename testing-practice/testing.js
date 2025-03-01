d293f5cde75a8ec0cd258df21cf958d352d7d10f
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-03-01 14:37:16 -0600

    Autoformat, reduce spacing

--- testing-practice/testing.js
+++ testing-practice/testing.js
@@ -1,24 +1,24 @@
 function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 function reverseString(str) {
-    return str.split("").reverse().join("");
+  return str.split("").reverse().join("");
 }
 
 const calculator = {
-    add(a, b) {
-        return a + b;
-    },
-    subtract(a, b) {
-        return a - b;
-    },
-    divide(a, b) {
-        return a / b;
-    },
-    multiply(a, b) {
-        return a * b;
-    }
-}
+  add(a, b) {
+    return a + b;
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+  divide(a, b) {
+    return a / b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
+};
 
-export {capitalize, reverseString, calculator};
+export { capitalize, reverseString, calculator };
