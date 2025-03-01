80672532df0d6608ab56224964b009a2068c1e19
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-03-01 14:41:41 -0600

    Add comments to testing file

--- testing-practice/testing.js
+++ testing-practice/testing.js
@@ -1,22 +1,31 @@
 function capitalize(str) {
+  // Capitalize the first character and concatenate with the rest of the string
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 function reverseString(str) {
+  // Split the string into an array, reverse it, and join it back into a string
   return str.split("").reverse().join("");
 }
 
 const calculator = {
   add(a, b) {
+    // Return the sum of a and b
     return a + b;
   },
+
   subtract(a, b) {
+    // Return the difference of a and b
     return a - b;
   },
+
   divide(a, b) {
+    // Return the quotient of a and b
     return a / b;
   },
+
   multiply(a, b) {
+    // Return the product of a and b
     return a * b;
   },
 };
