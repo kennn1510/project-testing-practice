c736ec09d942fb61faf94ec5a845e6e6c2ba6238
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-03-01 13:43:20 -0600

    Working reverseString that passes test

--- testing-practice/testing.js
+++ testing-practice/testing.js
@@ -2,4 +2,8 @@ function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-export { capitalize };
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+export { capitalize, reverseString };
--- testing-practice/testing.test.js
+++ testing-practice/testing.test.js
@@ -1,5 +1,9 @@
-import { capitalize } from "./testing";
+import { capitalize, reverseString } from "./testing";
 
 test("capitalize function takes a string and returns first character capitalized", () => {
   expect(capitalize("hello")).toBe("Hello");
+});
+
+test("Reverses string", () => {
+  expect(reverseString("hello")).toBe("olleh");
 });
